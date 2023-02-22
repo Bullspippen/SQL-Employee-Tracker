8b78b8120abfadf2adaed85fce6b13bec159eb91
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-02-22 00:25:54 -0500

    updated connection.js changed host, localhost to host, 127.0.0.1 to fix error when application would not run successfully

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mysql = require ("mysql2")
 
 // Create the connection to the database
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
     password: 'Mj2012!!!',
     database: 'employees_db',
