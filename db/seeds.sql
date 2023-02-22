4b78e229f6bef4be25384ec48da7ca2080266fd8
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-02-21 20:22:02 -0500

    added roles and eployees to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,11 +8,20 @@ INSERT INTO department (name) VALUES
 INSERT INTO role (title, salary, department_id) VALUES
     ("Account Executive", 75000, 1),
     ("Accountant", 60000, 2),
-    ("Director of Marketing", 100000, 3),
+    ("Markerting Researcher", 100000, 3),
     ("Software Engineer", 90000, 4);
+    ("Sales Manager", 120000, 1),
+    ("Finance Manager", 140000, 2),
+    ("Marketing Director", 160000, 3),
+    ("Engineering Manager", 830000, 4);
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-    ("Michael", "Smith", 1, NULL),
-    ("James", "Brown", 2, NULL),
-    ("Robert", "Hawkins", 3, NULL),
-    ("Penny", "Hardaway", 4, NULL);
+    ("Michael", "Smith", 1, 2),
+    ("James", "Brown", 2, 2),
+    ("Robert", "Hawkins", 3, 3),
+    ("Penny", "Hardaway", 4, 4);
+    ("Sarah", "Johnson", 2, 1)
+    ("Naomi", "Silver", 3, 2)
+    ("Britney", "Pearson", 4, 4)
+    ("Brett", "Hart",1, 1)
