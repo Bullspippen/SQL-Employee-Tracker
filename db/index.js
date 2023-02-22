4f4787ede31f8b2db96b2fc0c4097d5a3a42b273
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-02-21 20:00:55 -0500

    index.js updated to include references to employees, departments, roles, manager

--- db/index.js
+++ db/index.js
@@ -6,8 +6,88 @@ class DB{
     }
     findAllEmployees(){
         return this.connection.promise().query(
-            "select employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, concat(manager.first_name, ' ', manager.last_name) as manager from employee left join role on employee.role_id = role.id left join department on role.department_id = department.id left join employee manager on manager.id = employee.manager_id;" 
-        )
+            `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+            FROM employee 
+            LEFT JOIN role ON employee.role_id = role.id 
+            LEFT JOIN department ON role.department_id = department.id 
+            LEFT JOIN employee manager ON manager.id = employee.manager_id;`
+        );
+    }
+
+    findAllDepartments() {
+        return this.connection.promise().query(
+            `SELECT id, name 
+            FROM department`
+        );
+    }
+
+    findAllRoles() {
+        return this.connection.promise().query(
+            `SELECT role.id, role.title, department.name AS department, role.salary 
+            FROM role 
+            LEFT JOIN department ON role.department_id = department.id`
+        );
+    }
+
+    addEmployee(employee) {
+        const { first_name, last_name, role_id, manager_id } = employee;
+        return this.connection.promise().query(
+            `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES (?, ?, ?, ?)`,
+            [first_name, last_name, role_id, manager_id]
+        );
+    }
+
+    removeEmployee(employee_id) {
+        return this.connection.promise().query(
+            `DELETE FROM employee 
+            WHERE id = ?`,
+            [employee_id]
+        );
+    }
+
+    updateEmployeeRole(employee_id, role_id) {
+        return this.connection.promise().query(
+            `UPDATE employee 
+            SET role_id = ? 
+            WHERE id = ?`,
+            [role_id, employee_id]
+        );
+    }
+
+    updateEmployeeManager(employee_id, manager_id) {
+        return this.connection.promise().query(
+            `UPDATE employee 
+            SET manager_id = ? 
+            WHERE id = ?`,
+            [manager_id, employee_id]
+        );
+    }
+
+
+    findAllEmployeesByManager(manager_id) {
+        return this.connection.promise().query(
+            `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+            FROM employee 
+            LEFT JOIN role ON employee.role_id = role.id 
+            LEFT JOIN department ON role.department_id = department.id 
+            LEFT JOIN employee manager ON manager.id = employee.manager_id 
+            WHERE manager.id = ?`,
+            [manager_id]
+        );
+    }
+
+    findAllEmployeesByDepartment(department_id) {
+        return this.connection.promise().query(
+            `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+            FROM employee 
+            LEFT JOIN role ON employee.role_id = role.id 
+            LEFT JOIN department ON role.department_id = department.id 
+            LEFT JOIN employee manager ON manager.id = employee.manager_id 
+            WHERE department.id = ?`,
+            [department_id]
+        );
+    
     }
 }
 
