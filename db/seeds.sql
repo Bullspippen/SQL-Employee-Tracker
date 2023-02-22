358c6108e24e2e37fa249134762744323018e1cc
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-02-21 20:42:02 -0500

    updated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,25 +3,25 @@ INSERT INTO department (name) VALUES
     ("sales"), 
     ("finance"),
     ("marketing"),
-    ("enigneering");
+    ("engineering");
 
 INSERT INTO role (title, salary, department_id) VALUES
     ("Account Executive", 75000, 1),
     ("Accountant", 60000, 2),
-    ("Markerting Researcher", 100000, 3),
-    ("Software Engineer", 90000, 4);
-    ("Sales Manager", 120000, 1),
-    ("Finance Manager", 140000, 2),
-    ("Marketing Director", 160000, 3),
-    ("Engineering Manager", 830000, 4);
+    ("Marketing Researcher", 100000, 3),
+    ("Software Engineer", 90000, 4),
+    ("Sales Manager", 120000, 5),
+    ("Finance Manager", 140000, 6),
+    ("Marketing Director", 160000, 7),
+    ("Engineering Manager", 180000, 8);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-    ("Michael", "Smith", 1, 2),
-    ("James", "Brown", 2, 2),
-    ("Robert", "Hawkins", 3, 3),
-    ("Penny", "Hardaway", 4, 4);
-    ("Sarah", "Johnson", 2, 1)
-    ("Naomi", "Silver", 3, 2)
-    ("Britney", "Pearson", 4, 4)
-    ("Brett", "Hart",1, 1)
+    ("Michael", "Smith", 1, 5),
+    ("James", "Brown", 2, 6),
+    ("Robert", "Hawkins", 3, 7),
+    ("Penny", "Hardaway", 4, 8),
+    ("Sarah", "Johnson", 2, 5),
+    ("Naomi", "Silver", 3, 6),
+    ("Britney", "Pearson", 4, 8),
+    ("Brett", "Hart",1, 5);
